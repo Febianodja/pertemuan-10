4012885dc83f34d4f0074679b1393e4ced5cb64d
febianodja <febianodja@gmail.com>
2021-02-10 09:01:13 +0700

    latihan no.1

--- regex-exercise1.js
+++ regex-exercise1.js
@@ -1,11 +1,28 @@
 // LATIHAN 1
-function isUppercase(words){
-    let regex1 = /[A-Z]/;
-    if(regex1.test(words) == true){
-        return 'Huruf pertama adalah huruf besar';
+// function isUppercase(words){
+    // let regex1 = /[[A-Z][a-z0-9_-]/;
+    //     if(regex1.test(words) == true){
+        //         return 'Huruf pertama adalah huruf besar';
+        //     } else {
+            //         return 'Huruf pertama adalah huruf kecil';
+            //     }
+            // }
+            // console.log(isUppercase("Haha"));
+            // console.log(isUppercase("hihi"));
+// console.log(isUppercase("hii"));
+
+function validation(){
+    let userInput = document.getElementById("validasiHuruf").value;
+    let showResult = document.querySelector("#result");
+    
+    //buat validasi regex
+    let karakterRegex = /^[A-Z]/;
+    
+    if (karakterRegex.test(userInput)) {
+      showResult.innerHTML = `Huruf pertama adalah huruf <b>besar</b>`;
+      
     } else {
-        return 'Huruf pertama adalah huruf kecil';
+      showResult.innerHTML = `Huruf pertama adalah huruf <b>kecil</b>`;
     }
+    document.getElementById("validasiHuruf").value = "";
 }
-console.log(isUppercase("Haha"));
-console.log(isUppercase("hihi"));
